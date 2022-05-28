7832ff5ad597fc0cd081f099d3d0d9ffdd3621a2
Ulrich Klug <u.klug@ulrichklug.com>
2022-05-28 15:14:47 +0200

    Add scripts to package.json

---
+++ icon-dist/icons.ts
@@ -0,0 +1,109 @@
+export type IconsId =
+  | "alpha-lc"
+  | "beta-lc"
+  | "chi-lc"
+  | "delta-lc"
+  | "epsilon-lc"
+  | "eta-lc"
+  | "gamma-lc"
+  | "kappa-lc"
+  | "lambda-lc"
+  | "lota-lc"
+  | "mu-lc"
+  | "nu-lc"
+  | "omega-lc"
+  | "omicron-lc"
+  | "phi-lc"
+  | "pi-lc"
+  | "psi-lc"
+  | "rho-lc"
+  | "sigma-lc"
+  | "t"
+  | "tau-lc"
+  | "theta-lc"
+  | "upsilon-lc"
+  | "xi-lc"
+  | "zeta-lc";
+
+export type IconsKey =
+  | "AlphaLc"
+  | "BetaLc"
+  | "ChiLc"
+  | "DeltaLc"
+  | "EpsilonLc"
+  | "EtaLc"
+  | "GammaLc"
+  | "KappaLc"
+  | "LambdaLc"
+  | "LotaLc"
+  | "MuLc"
+  | "NuLc"
+  | "OmegaLc"
+  | "OmicronLc"
+  | "PhiLc"
+  | "PiLc"
+  | "PsiLc"
+  | "RhoLc"
+  | "SigmaLc"
+  | "T"
+  | "TauLc"
+  | "ThetaLc"
+  | "UpsilonLc"
+  | "XiLc"
+  | "ZetaLc";
+
+export enum Icons {
+  AlphaLc = "alpha-lc",
+  BetaLc = "beta-lc",
+  ChiLc = "chi-lc",
+  DeltaLc = "delta-lc",
+  EpsilonLc = "epsilon-lc",
+  EtaLc = "eta-lc",
+  GammaLc = "gamma-lc",
+  KappaLc = "kappa-lc",
+  LambdaLc = "lambda-lc",
+  LotaLc = "lota-lc",
+  MuLc = "mu-lc",
+  NuLc = "nu-lc",
+  OmegaLc = "omega-lc",
+  OmicronLc = "omicron-lc",
+  PhiLc = "phi-lc",
+  PiLc = "pi-lc",
+  PsiLc = "psi-lc",
+  RhoLc = "rho-lc",
+  SigmaLc = "sigma-lc",
+  T = "t",
+  TauLc = "tau-lc",
+  ThetaLc = "theta-lc",
+  UpsilonLc = "upsilon-lc",
+  XiLc = "xi-lc",
+  ZetaLc = "zeta-lc",
+}
+
+export const ICONS_CODEPOINTS: { [key in Icons]: string } = {
+  [Icons.AlphaLc]: "61697",
+  [Icons.BetaLc]: "61698",
+  [Icons.ChiLc]: "61699",
+  [Icons.DeltaLc]: "61700",
+  [Icons.EpsilonLc]: "61701",
+  [Icons.EtaLc]: "61702",
+  [Icons.GammaLc]: "61703",
+  [Icons.KappaLc]: "61704",
+  [Icons.LambdaLc]: "61705",
+  [Icons.LotaLc]: "61706",
+  [Icons.MuLc]: "61707",
+  [Icons.NuLc]: "61708",
+  [Icons.OmegaLc]: "61709",
+  [Icons.OmicronLc]: "61710",
+  [Icons.PhiLc]: "61711",
+  [Icons.PiLc]: "61712",
+  [Icons.PsiLc]: "61713",
+  [Icons.RhoLc]: "61714",
+  [Icons.SigmaLc]: "61715",
+  [Icons.T]: "61716",
+  [Icons.TauLc]: "61717",
+  [Icons.ThetaLc]: "61718",
+  [Icons.UpsilonLc]: "61719",
+  [Icons.XiLc]: "61720",
+  [Icons.ZetaLc]: "61721",
+};
